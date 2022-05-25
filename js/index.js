b4b2a5f8c7a8f7e1cc7ff71be555b6b549a5b5d3
Lucas Yang <yangchenshin77@gmail.com>
2022-05-26 00:20:27 +0800

    Tweak

--- js/index.js
+++ js/index.js
@@ -6,7 +6,30 @@ import 'slick-carousel-latest/slick/slick-theme.css'
 import '../scss/bootstrap.scss'
 import '../scss/style.scss'
 
-$(function(){
+window.addEventListener('hashchange', function (event) {
+  event.preventDefault();
+  const url = location.hash.substr(1);
+  const target = document.querySelector(`.${url}`).offsetTop - 60;
+  window.scrollTo({
+    top: target,
+    left: 0,
+    behavior: 'smooth' // => 滑動效果
+  });
+});
+
+/*
+nav 縮小
+from: https://benmarshall.me/attaching-javascript-handlers-to-scroll-events/
+*/
+document.addEventListener('wheel', () => {
+  if (window.scrollY > 50) {
+    document.querySelector('.nav').classList.add('nav-scrolled');
+  } else {
+    document.querySelector('.nav').classList.remove('nav-scrolled');
+  }
+}, { capture: false, passive: true });
+
+$(function () {
 
   new Typed('#typed', {
     strings: ['你知道嗎？<br>拖延...<br><br>可以拯救世界喔'],
@@ -46,27 +69,5 @@ $(function(){
     ]
   });
 
-  window.addEventListener("hashchange", function (event) {
-    event.preventDefault();
-    const url = location.hash.substr(1);
-    const target = document.querySelector(`.${url}`).offsetTop - 60;
-    window.scrollTo({
-      top: target,
-      left: 0,
-      behavior: 'smooth' // => 滑動效果
-    });
-  });
-
-  /*
-  nav 縮小
-  from: https://benmarshall.me/attaching-javascript-handlers-to-scroll-events/
-  */
-  document.addEventListener('wheel', () => {
-    if (window.scrollY > 50) {
-      document.querySelector('.nav').classList.add('nav-scrolled')
-    } else {
-      document.querySelector('.nav').classList.remove('nav-scrolled')
-    }
-  }, { capture: false, passive: true })
 });
 
